d10fb62bb45fd5bd52ab29442a7d7c37b94ba373
annakulova <aakulovaa03@gmail.com>
2025-04-21 16:40:31 +0300

    create repositories

---
+++ src/main/java/com/todo/todoList/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.todo.todoList.repository;
+
+import com.todo.todoList.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findBuUserId(Long userId);
+}
