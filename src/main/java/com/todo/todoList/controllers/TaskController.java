c0f4764cab998fa4737c83b5c4fb40fdbe0e9d32
annakulova <aakulovaa03@gmail.com>
2025-04-21 17:26:19 +0300

    create controller

---
+++ src/main/java/com/todo/todoList/controllers/TaskController.java
@@ -0,0 +1,54 @@
+package com.todo.todoList.controllers;
+
+import com.todo.todoList.dto.TaskDTO;
+import com.todo.todoList.models.Task;
+import com.todo.todoList.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/task")
+public class TaskController {
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> findAllTasks(){
+        List<Task> tasks = taskService.findAllTasks();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Task>> findTasksByUserId(@PathVariable Long userId){
+        List<Task> tasks = taskService.findTasksByUserId(userId);
+        return ResponseEntity.ok(tasks);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Task> saveTask(@RequestBody TaskDTO taskDTO){
+        Task createdTask = taskService.saveTask(taskDTO);
+        return ResponseEntity.ok(createdTask);
+    }
+
+    @PutMapping("/update/{taskId}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long taskId, @RequestBody TaskDTO taskDTO){
+        Task updatedTask  = taskService.updateTask(taskId, taskDTO);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/delete/{task}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId){
+        taskService.deleteTask(taskId);
+        return ResponseEntity.noContent().build();
+    }
+
+}
