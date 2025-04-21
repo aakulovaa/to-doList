9db3ef0cdd02958f2ea9345c05e9222c8c3f9dca
annakulova <aakulovaa03@gmail.com>
2025-04-21 23:57:02 +0300

    create TaskCommentDTO.java

---
+++ src/main/java/com/todo/todoList/dto/TaskCommentDTO.java
@@ -0,0 +1,40 @@
+package com.todo.todoList.dto;
+
+public class TaskCommentDTO {
+    private Long id;
+    private String content;
+    private Long taskId;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
