bffa03350e39a2791ac8386306b1f964c9c6ed23
annakulova <aakulovaa03@gmail.com>
2025-04-21 16:36:53 +0300

    create DTO-class
    для избежания проблем на сервере при создании/обновлении элементов - обращение по id к пользователю

---
+++ src/main/java/com/todo/todoList/dto/TaskDTO.java
@@ -0,0 +1,10 @@
+package com.todo.todoList.dto;
+
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String priority;
+    private String status;
+    private Long userId;
+}
